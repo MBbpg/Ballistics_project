95bce07ff7a55e114ac21a43b2eec2e6bdaf62d8
Mohácsi Barnabás <koviubi0909@gmail.com>
2023-10-04 13:22:49 +0200

    Updated the menu program with the "sure" function.

--- Ballistics.cpp
+++ Ballistics.cpp
@@ -1,107 +1,61 @@
-#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS /**To avoid warnings in Visual Studio**/
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
+int sure() /**A function that asks the user whether or not he wants to commit.**/
+{
+    int choice, end = 0; /**The variable that stores the users choice, and the variable that keeps the algorithm running until
+    the user inputs a valid choice**/
+    while (end == 0)
+    {
+        printf("Are you sure you wish to proceed?\n");
+        printf("  1. Yes\n  2. No\n");
+        printf("Please enter the number of your choice: ");
+        scanf("%d", &choice);
+        printf("\n");
+        switch (choice)
+        {
+        case 1: return 0; /**Knowing that the function returns a 0, we can use it to e.g. stop an infinite loop**/
+        case 2: return 1; /**Knowing that the function returns a 1, we can adjust our loops other loops so that they keep running**/
+        default: printf("Please enter either 1 or 2\n"); /**If the user entered an invalid choice, we prompt him to enter a valid one**/
+        }
+    }
+}
+
 
 int main()
-{	
-	printf("This is a program designed to calculate the firing angle for your cannon.\nYou will be able to give certain parameters to adjust the firing process to your needs.\nYou may now choose how to proceed next:\n");
-	printf("(You will be returned to this menu after your choices, unless you choose to exit or launch the program.)\n");
-	/*We need here the infinite while, until either the user exits, or decides that he has given all data and wishes to actually run the calculations.*/
-	int end = 0; /*We will change it when the menu ends due to either the user launching the calculations, or due to the user exiting*/
-	int choice = 0; /*This variable will be used to read in the choice of the user*/
-	int yesorno = 0; /*We will use this variable when asking a yes or no question from the user*/
-	int second_end = 0; /*Another variable to use for ending a while algorithm*/
-	while (end < 1)
-	{
-		printf("  1. I would like to give information about the position of my target.\n  2. I would like to give information about the current wind.\n  3. I would like to give information about the projectile of the cannon.\n  4. I would like the program to begin the calculations.\n  5. I would like to exit the program.\n");
-		printf("Please enter the number of your choice: ");
-		scanf("%d", &choice);
-		printf("\n");
-		if (choice == 1)
-		{
+{
+    printf("This is a program designed to calculate the firing angle for your cannon.\nYou will be able to give certain parameters to adjust the firing process to your needs.\nYou may now choose how to proceed next:\n");
+    printf("(You will be returned to this menu after your choices, unless you choose to exit or launch the program.)\n");
+    int choice, end = 1; /**The choice of the user and the variable that keeps the algorithm running until the user decides to end
+    the program, or run it**/
 
-		}
-		else if (choice == 2)
-		{
-
-		}
-		else if (choice == 3)
-		{
-
-		}
-		else if (choice == 4)
-		{
-			printf("Are you sure you would like to launch the program?\n");
-			printf("  1. Yes\n  2. No\n");
-			printf("Please enter the number of your choice: ");
-			scanf("%d", &yesorno);
-			printf("\n");
-			second_end = 0;
-			while (second_end < 1)
-			{
-				if (yesorno == 1)
-				{
-					end = 2;
-					second_end = 1;
-					printf("The calculations will begin now. You may see the results.\n");
-
-				}
-				else if (yesorno == 2)
-				{
-					second_end = 1;
-					printf("You are redirected to the menu now.\n\n");
-				}
-				else
-				{
-					printf("Please enter a valid choice, either 1 or 2: ");
-					scanf("%d", &yesorno);
-					printf("\n");
-				}
-
-			}
-		}
-		else if (choice == 5)
-		{
-			printf("Are you sure you would like to end the program?\n");
-			printf("  1. Yes\n  2. No\n");
-			second_end = 0;
-			printf("Please enter the number of your choice: ");
-			scanf("%d", &yesorno);
-			printf("\n");
-			while (second_end < 1)
-			{
-				if (yesorno == 1)
-				{
-					end = 2;
-					second_end = 1;
-					printf("Thank you for trying the program! Have a great time!\n");
-
-				}
-				else if (yesorno == 2)
-				{
-					second_end = 1;
-					printf("You are redirected to the menu now.\n\n");
-				}
-				else
-				{
-					printf("Please enter a valid choice, either 1 or 2: ");
-					scanf("%d", &yesorno);
-					printf("\n");
-				}
-			}
-
-
-		}
-		else
-		{
-			printf("Please enter a valid choice, ranging from 1 to 5.\n");
-			printf("You are redirected to the menu now.\n\n");
-		}
-	}
+    /**The main menu that is being displayed**/
+    while (end == 1)
+    {
+        printf("Main menu:\n  1. I would like to give information about the position of my target.\n  2. I would like to give information about the current wind.\n  3. I would like to give information about the projectile of the cannon.\n  4. I would like the program to begin the calculations.\n  5. I would like to exit the program.\n");
+        printf("Please enter the number of your choice: ");
+        scanf("%d", &choice);
+        printf("\n");
+        switch (choice)
+        {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4: end = sure(); /**Depending on the users choice, either the program ends, and the calculations begin, or it just returns to the menu**/
+            break;
+        case 5: end = sure(); /**Depending on the users choice, either the program ends, or it just returns to the menu**/
+            break;
+        default: printf("Please enter a number ranging from 1 to 5\n\n"); /**If the user entered an invalid choice, we prompt him to enter a valid one**/
+        }
+    }
+    printf("Thank you for using the program!\n");
 
 
 
-	return 0;
+    return 0;
 }
