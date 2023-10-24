61290220f0357b4f80c8005e009be131ffc1bb0f
Mohácsi Barnabás <koviubi0909@gmail.com>
2023-10-24 22:01:02 +0200

    The calculations are half-ready. We do not get the desired results, for unknown reasons.

--- Ballistics.cpp
+++ Ballistics.cpp
@@ -1,9 +1,10 @@
 #define _CRT_SECURE_NO_WARNINGS /**To avoid warnings in Visual Studio**/
+#define _USE_MATH_DEFINES /*To use the M_PI from math.h*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
-int sure() /**A function that asks the user whether or not he wants to commit.**/
+int sure(void) /**A function that asks the user whether or not he wants to commit (like when he wants to exit or launch the calculations)**/
 {
     int choice, end = 0; /**The variable that stores the users choice, and the variable that keeps the algorithm running until
     the user inputs a valid choice**/
@@ -23,6 +24,94 @@ int sure() /**A function that asks the user whether or not he wants to commit.**
     }
 }
 
+double read_data(void) /*A function that will be used to read in the data from the user necessary for calculations*/
+{
+    double data = 0;
+    scanf("%lf", &data);
+    return data;
+}
+
+double is_positive() /*We will use this function to check whether or not the value is positive, and if it is not, we prompt the user
+                                to enter another value until the user enters a positive one. That positive value then gets returned.*/
+{   
+    double data = read_data();
+    while (1)
+    {
+        if (data <= 0)
+        {
+            printf("Please enter a number greater than 0!\n");
+            data = read_data();
+        }
+        else
+            break;
+    }
+    return data;
+}
+
+void calculations(double target, double wind, double v0, double m, double A)
+{
+    double rho = 1.293; /**The density of air, required for the calculations**/
+    double Cd = 0.295; /**The drag coefficient of a sphere, required for the calculations**/
+    double g = 9.81; /**Gravitational acceleration, required for the calculations**/
+    double vterminal = sqrt(m * g / (rho * Cd * A * 0.5)); /*The terminal velocity while falling, required for the calculations*/
+    double dt = 0.0001; /*A "delta t", timestep which we will use for the numerical calculations. 0.0001 is sufficiently small.*/
+    double drag, h1, h2, htop, hterm, ttop, tland, vx, vy, acc, displacement, t; /**/
+    for (int i = 0; i <= 90; i += 1)
+    {
+        h1 = 0, h2 = 0, htop = 0, hterm = 0, ttop = 0, tland = 0, vx = v0 * cos(i * M_PI / 180), vy = v0 * sin(M_PI / 180), displacement = 0, t = 0;
+        while (tland == 0)
+        {
+            if (h2 < 0) /*If during falling we encounter a negative height, we have touched the ground*/
+                tland = t;
+            t += dt;
+            if (vy > 0) /*This runs until the projectile gets slowed down below 0*/
+            {
+                drag = rho * Cd * A * 0.5 * pow(vy, 2);
+                acc = -1 * (g + drag/m); /*The acceleration is calculated from the sum of the gravitational acc. and the acc. from the drag force*/
+                vy += acc * dt;
+                h1 += vy * dt;
+            }
+            else if (vy < 0 && htop == 0) /*If this is true, we have reached the top for the first time during a flight*/
+            {
+                ttop = t;
+                htop = h1;
+                h2 = h1;
+                vy = 0;
+            }
+            else if (ttop != 0 && hterm == 0) /*If this is true, we have reached the top before, but we haven't reached terminal velocity yet*/
+            {
+                drag = rho * Cd * A * 0.5 * pow(vy, 2);
+                acc = g - (drag / m);
+                vy += acc * dt;
+                h2 -= vy * dt;
+                if (acc < 0.00001)/*If we reach an acceleration of 0.00001, we are safe to say that we have reached a point where further acceleration is negligible*/
+                {
+                    hterm = h2;
+                }
+            }
+            else if (hterm != 0)/*We will use this after we reach terminal velocity, as then we no longer accelerate*/
+            {
+                h2 -= vterminal * dt;
+            }
+
+        }
+        t = 0;
+        while (t <= tland)
+        {
+            t += dt;
+            if (vx >= 0)
+            {
+                drag = rho * Cd * A * 0.5 * pow(vx-wind, 2);/*We need to subtract the wind, because the user enters a negative value in case of headwind, and a positive in the case of tailwind,
+                                                            however if there is a headwind, our relative velocity to the wind adds up, and with a tailwind it gets subtracted from the velocity of the wind*/
+                acc = -1 * drag / m;
+                vx += acc * dt;
+                displacement += vx * dt;
+            }
+        }
+        printf("%d: %f\n", i, displacement);
+    }
+}
+
 
 int main()
 {
@@ -30,27 +119,43 @@ int main()
     printf("(You will be returned to this menu after your choices, unless you choose to exit or launch the program.)\n");
     int choice, end = 1; /**The choice of the user and the variable that keeps the algorithm running until the user decides to end
     the program, or run it**/
+    double target = 0; /*The distance of the target, we will ask the user to enter it*/
+    double wind = 0; /*The speed of wind, we will ask the user to enter it*/
+    double v0 = 0; /**The starting velocity, we will ask the user to enter it**/
+    double m = 0; /**The mass of the projectile, we will ask the user to enter it**/
+    double A = 0; /**The cross sectional area of the projectile, we will ask the user to enter it**/
 
     /**The main menu that is being displayed**/
     while (end == 1)
     {
-        printf("Main menu:\n  1. I would like to give information about the position of my target.\n  2. I would like to give information about the current wind.\n  3. I would like to give information about the projectile of the cannon.\n  4. I would like the program to begin the calculations.\n  5. I would like to exit the program.\n");
+        printf("\nMain menu:\n  1. I would like to give information about the position of my target.\n  2. I would like to give information about the current wind.\n  3. I would like to give information about the mass of the projectile.\n  4. I would like to give information about the area of the projectile\n  5. I would like to give information about the muzzle velocity of the cannon.\n  6. I would like the program to begin the calculations.\n  7. I would like to exit the program.\n");
         printf("Please enter the number of your choice: ");
         scanf("%d", &choice);
         printf("\n");
         switch (choice)
         {
-        case 1:
+        case 1: printf("Please enter the distance of the target in meters!\n");
+            target = is_positive();
             break;
-        case 2:
+        case 2: printf("Please enter the speed of wind in m/s!\nEnter 0 if there is no wind, and enter a negative number to indicate headwind!\n");
+            wind = read_data();
             break;
-        case 3:
+        case 3: printf("Please enter the mass of the projectile in kg!\n");
+            m = is_positive();
             break;
-        case 4: end = sure(); /**Depending on the users choice, either the program ends, and the calculations begin, or it just returns to the menu**/
+        case 4: printf("Please enter the area of the projectile in m^2!\n");
+            A = is_positive();
             break;
-        case 5: end = sure(); /**Depending on the users choice, either the program ends, or it just returns to the menu**/
+        case 5: printf("Please enter the starting velocity in m/s!\n");
+            v0 = is_positive();
             break;
-        default: printf("Please enter a number ranging from 1 to 5\n\n"); /**If the user entered an invalid choice, we prompt him to enter a valid one**/
+        case 6: end = sure(); /**Depending on the users choice, either the program ends, and the calculations begin, or it just returns to the menu**/
+            if (end == 0)
+                calculations(target, wind, v0, m, A);
+            break;
+        case 7: end = sure(); /**Depending on the users choice, either the program ends, or it just returns to the menu**/
+            break;
+        default: printf("Please enter a number ranging from 1 to 7\n\n"); /**If the user entered an invalid choice, we prompt him to enter a valid one**/
         }
     }
     printf("Thank you for using the program!\n");
