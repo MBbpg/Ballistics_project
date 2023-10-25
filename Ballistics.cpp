329103f8babce183f962f7b3a64d7fe60c84ffe7
Mohácsi Barnabás <koviubi0909@gmail.com>
2023-10-25 15:51:08 +0200

    Redone Calc, created Dist, so that we can easily compute up to further accuracy

--- Ballistics.cpp
+++ Ballistics.cpp
@@ -48,70 +48,101 @@ double is_positive() /*We will use this function to check whether or not the val
     return data;
 }
 
-void calculations(double target, double wind, double v0, double m, double A)
+double difference(double a, double b) /*We will use this function to calculate the difference between the displacement 
+                                      of the projectile with a given angle, and the target distance*/
+{
+    return fabs(a - b);
+}
+
+void distance(int i, double target, double wind, double v0, double m, double A, double **angle1, double **angle2, double *min1, double *min2)
 {
     double rho = 1.293; /**The density of air, required for the calculations**/
     double Cd = 0.295; /**The drag coefficient of a sphere, required for the calculations**/
     double g = 9.81; /**Gravitational acceleration, required for the calculations**/
     double vterminal = sqrt(m * g / (rho * Cd * A * 0.5)); /*The terminal velocity while falling, required for the calculations*/
-    double dt = 0.001; /*A "delta t", timestep which we will use for the numerical calculations. 0.001 is sufficiently small.*/
-    double drag, h1, h2, htop, hterm, ttop, tland, vx, vy, acc, displacement, t; /**/
+    double dt = 0.0001; /*A "delta t", timestep which we will use for the numerical calculations. 0.001 is sufficiently small.*/
+    double drag, h1, h2, htop, hterm, ttop, tland, vx, vy, acc, displacement, t;
+    h1 = 0, h2 = 0, htop = 0, hterm = 0, ttop = 0, tland = 0, vx = v0 * cos(i * M_PI / 180), vy = v0 * sin(i * M_PI / 180), displacement = 0, t = 0;
+    while (tland == 0)
+    {
+        if (h2 < 0) /*If during falling we encounter a negative height, we have touched the ground*/
+            tland = t;
+        t += dt;
+        if (i == 0)/*Launching at 0 degrees means we land basically instantly*/
+            tland = t;
+        if (vy > 0 && ttop == 0) /*This runs until the projectile gets slowed down below 0*/
+        {
+            drag = rho * Cd * A * 0.5 * pow(vy, 2);
+            acc = -1 * (g + drag / m); /*The acceleration is calculated from the sum of the gravitational acc. and the acc. from the drag force*/
+            vy += acc * dt;
+            h1 += vy * dt;
+        }
+        else if (vy <= 0 && ttop == 0) /*If this is true, we have reached the top for the first time during a flight*/
+        {
+            ttop = t;
+            htop = h1;
+            h2 = h1;
+            vy = 0;
+        }
+        else if (ttop != 0 && hterm == 0) /*If this is true, we have reached the top before, but we haven't reached terminal velocity yet*/
+        {
+            drag = rho * Cd * A * 0.5 * pow(vy, 2);
+            acc = g - (drag / m);
+            vy += acc * dt;
+            h2 -= vy * dt;
+            if (acc < 0.000001)/*If we reach an acceleration of 0.000001, we are safe to say that we have reached a point where further acceleration is negligible*/
+            {
+                hterm = h2;
+            }
+        }
+        else if (hterm != 0)/*We will use this after we reach terminal velocity, as then we no longer accelerate*/
+        {
+            h2 -= vterminal * dt;
+        }
+
+    }
+    t = 0;
+    while (t <= tland)
+    {
+        t += dt;
+        if (vx >= 0)
+        {
+            drag = rho * Cd * A * 0.5 * pow(vx - wind, 2);/*We need to subtract the wind, because
+                                                        the user enters a negative value in case of headwind,
+                                                        and a positive in the case of tailwind. However if there is a headwind,
+                                                        our relative velocity to the wind adds up, and with a tailwind it gets subtracted from the velocity of the wind*/
+            acc = -1 * drag / m;
+            vx += acc * dt;
+            displacement += vx * dt;
+        }
+    }
+    printf("%d: %f\n", i, displacement);
+    if (difference(displacement, target) < *min1)
+    {
+        *min1 = difference(displacement, target);
+        **angle1 = i;
+    }
+    else if (difference(displacement, target) < *min2)
+    {
+        *min2 = difference(displacement, target);
+        **angle2 = i;
+    }
+}
+
+
+void calculations(double target, double wind, double v0, double m, double A, double *angle1, double *angle2)
+{
+    double rho = 1.293; /**The density of air, required for the calculations**/
+    double Cd = 0.295; /**The drag coefficient of a sphere, required for the calculations**/
+    double g = 9.81; /**Gravitational acceleration, required for the calculations**/
+    double vterminal = sqrt(m * g / (rho * Cd * A * 0.5)); /*The terminal velocity while falling, required for the calculations*/
+    double dt = 0.0001; /*A "delta t", timestep which we will use for the numerical calculations. 0.001 is sufficiently small.*/
+    double drag, h1, h2, htop, hterm, ttop, tland, vx, vy, acc, displacement, t, min1=target, min2=target; /**/
     for (int i = 1; i <= 90; i += 1)
     {
-        h1 = 0, h2 = 0, htop = 0, hterm = 0, ttop = 0, tland = 0, vx = v0 * cos(i * M_PI / 180), vy = v0 * sin(i * M_PI / 180), displacement = 0, t = 0;
-        while (tland == 0)
-        {
-            if (h2 < 0) /*If during falling we encounter a negative height, we have touched the ground*/
-                tland = t;
-            t += dt;
-            if (vy > 0 && ttop==0) /*This runs until the projectile gets slowed down below 0*/
-            {
-                drag = rho * Cd * A * 0.5 * pow(vy, 2);
-                acc = -1 * (g + drag/m); /*The acceleration is calculated from the sum of the gravitational acc. and the acc. from the drag force*/
-                vy += acc * dt;
-                h1 += vy * dt;
-            }
-            else if (vy < 0 && ttop == 0) /*If this is true, we have reached the top for the first time during a flight*/
-            {
-                ttop = t;
-                htop = h1;
-                h2 = h1;
-                vy = 0;
-            }
-            else if (ttop != 0 && hterm == 0) /*If this is true, we have reached the top before, but we haven't reached terminal velocity yet*/
-            {
-                drag = rho * Cd * A * 0.5 * pow(vy, 2);
-                acc = g - (drag / m);
-                vy += acc * dt;
-                h2 -= vy * dt;
-                if (acc < 0.00001)/*If we reach an acceleration of 0.00001, we are safe to say that we have reached a point where further acceleration is negligible*/
-                {
-                    hterm = h2;
-                }
-            }
-            else if (hterm != 0)/*We will use this after we reach terminal velocity, as then we no longer accelerate*/
-            {
-                h2 -= vterminal * dt;
-            }
-
-        }
-        t = 0;
-        while (t <= tland)
-        {
-            t += dt;
-            if (vx >= 0)
-            {
-                drag = rho * Cd * A * 0.5 * pow(vx-wind, 2);/*We need to subtract the wind, because 
-                                                            the user enters a negative value in case of headwind, 
-                                                            and a positive in the case of tailwind. However if there is a headwind, 
-                                                            our relative velocity to the wind adds up, and with a tailwind it gets subtracted from the velocity of the wind*/
-                acc = -1 * drag / m;
-                vx += acc * dt;
-                displacement += vx * dt;
-            }
-        }
-        printf("%d: %f\n", i, displacement);
+        distance(i, target, wind, v0, m, A, &angle1, &angle2, &min1, &min2);
     }
+    printf("%f, %f\n", *angle1, *angle2);
 }
 
 
@@ -126,6 +157,7 @@ int main()
     double v0 = 0; /**The starting velocity, we will ask the user to enter it**/
     double m = 0; /**The mass of the projectile, we will ask the user to enter it**/
     double A = 0; /**The cross sectional area of the projectile, we will ask the user to enter it**/
+    double angle1 = 0, angle2 = 0;
 
     /**The main menu that is being displayed**/
     while (end == 1)
@@ -153,7 +185,7 @@ int main()
             break;
         case 6: end = sure(); /**Depending on the users choice, either the program ends, and the calculations begin, or it just returns to the menu**/
             if (end == 0)
-                calculations(target, wind, v0, m, A);
+                calculations(target, wind, v0, m, A, &angle1, &angle2);
             break;
         case 7: end = sure(); /**Depending on the users choice, either the program ends, or it just returns to the menu**/
             break;
